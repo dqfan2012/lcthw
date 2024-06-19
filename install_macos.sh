8b4a292c5d362cf8fa70ab03c3283edffb0e571e
Samuel Stidham <dqfan2012@gmail.com>
2024-06-19 17:45:56 -0400

    Removed the unnecessary install_linux.sh and install_macos.sh files.

--- install_macos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-brew update
-brew install cppcheck valgrind clang llvm flawfinder
-brew install openjdk@17 postgresql@16
-brew install sonarqube
-brew install --cask sonar-scanner
-brew install infer
